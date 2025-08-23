0d7b5115fbdb952c12588f45da4f0aab7aac4b18
Harish Shaikh <harishshaikh26112005@gmail.com>
2025-08-23 10:24:27 +0530

    Update hello world.c

--- calculator.c
+++ calculator.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    char op;
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    printf("Enter operator (+, -, *, /): ");
-    scanf(" %c", &op);
-
-    if (op == '+')
-        printf("%d\n", a + b);
-    else if (op == '-')
-        printf("%d\n", a - b);
-    else if (op == '*')
-        printf("%d\n", a * b);
-    else if (op == '/')
-        printf("%d\n", a / b);
-
+    printf("Hello, World!\n");
     return 0;
 }
+
+ 
